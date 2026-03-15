d82c3c6dbb39ac15dff679da4a99e483241d780f
nuwandavek <vivekaithal44@gmail.com>
2026-03-15 13:05:46 -0700

    remove custom grad lib

--- quectrograd/index.js
+++
@@ -1,7 +0,0 @@
-// quectrograd public API
-export { Tensor } from './tensor.js';
-export { backward, zeroGrad, clearTape, tape } from './autograd.js';
-export { Adam } from './optim.js';
-export * as ops from './ops.js';
-export { initCPU } from './backend_cpu.js';
-export { initWebGPU } from './backend_webgpu.js';
